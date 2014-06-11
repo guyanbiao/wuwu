e83742b9b6952a72d2b3f208459007c04c42e83e
neal gu <neal@hah.com>
2014-06-11 12:16:26 +0800

    Initial Commit

---
+++ myFirstSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFirstSwift
+//
+//  Created by neal gu on 14-6-11.
+//  Copyright (c) 2014年 seven digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
