18f48c80c1fb7e684001400f8d88ed668140dec0
neal gu <neal@hah.com>
2014-06-29 13:03:55 +0800

    app init

---
+++ myFirstSwift/MyPlayground.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func login(a: String, b:String) -> Bool {
+    return false
+}
+
+login("", "")
+
+var b = "ssssdd"
+
+class Abc {
+    var name = b
+    let oo = "sss"
+}
+
+
+
+Abc().name
--- myFirstSwift/ViewController.swift
+++ myFirstSwift/ViewController.swift
@@ -8,7 +8,61 @@
 
 import UIKit
 
+let SeverUrl = "127.0.0.1:3000"
+
+func login(a: String, b: Int) -> Bool {
+    var url = NSURL(string: "http://\(SeverUrl)/user/sessions")
+    var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/users/sign_in"))
+
+    var session = NSURLSession.sharedSession()
+    request.HTTPMethod = "POST"
+    var params = ["user[username]":"jameson", "user[password]":"password"] as Dictionary
+    var err: NSError?
+    request.HTTPBody = "user[email]=guyanbiao@163.com&user[password]=gg785236".dataUsingEncoding(NSUTF8StringEncoding)
+    
+
+    var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+        println("Response: \(response)")
+    })
+    
+    task.resume()
+
+    
+    
+    
+
+    return false
+}
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var email : UITextField
+    @IBOutlet var password : UITextField
+    override func supportedInterfaceOrientations() -> Int {
+        return 2
+        //TODO
+    }
+    
+
+    
+    func halfOpenRangeLength(start: Int, end: Int) -> Int {
+        return end - start
+    }
+
+
+    
+    @IBAction func abc(sender : AnyObject) {
+        var emailText = email.text
+        var passwordText = password.text
+        println("sssss")
+        login("", 1)
+    }
+    
+    
+
+
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
